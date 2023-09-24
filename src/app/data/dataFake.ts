c1278c58657b0cb3599e8e170417d6e55ae39ad9
lolaleid <leidiane.igtu@gmail.com>
2023-09-24 17:38:08 -0300

    adding dynamic pages

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+    {
+        "id":  '1',
+        "photoCover": 'https://wallpapercave.com/wp/wp11431911.jpg',
+        "title": 'Solo Leveling em 2023?',
+        'description': 'Produtora promete liberar primeira temporada ainda esse ano.'    
+    },
+    {
+        "id":  '2',
+        "photoCover": 'https://www.animegeek.com/wp-content/uploads/2022/07/Mashle-manga-cover-image.jpg',
+        "title": 'Lançamento de Mashile: Magia e Músculos',
+        'description': 'Descrição do Post'    
+    },
+    {
+        "id":  '3',
+        "photoCover": 'https://cdn.beahero.gg/2022/12/Mushoku-Tensei-presentan-un-nuevo-visual-del-anime-de-cara-a-su-segunda-temporada.gif',
+        "title": '2º Temporada de Mushoku Tensei: Jobless Reincarnation',
+        'description': 'Descrição do Post'    
+    },
+    {
+        "id":  '4',
+        "photoCover": 'https://aniplus.fun/storage/blog-post/3/EI8rriCJkCE1pf6NPtLG49FujMdWrSVpPEHCR3ta.png',
+        "title": '2ª Temporada Confirmada!!!',
+        'description': 'Descrição do Post'    
+    }
+]
